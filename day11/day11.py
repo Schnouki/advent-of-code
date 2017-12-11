688352c8b31610dd7de8692c07ba425f8ae13993
Thomas Jost <schnouki@schnouki.net>
2017-12-11 14:11:54 +0100

    Day 11 part 2

--- day11/day11.py
+++ day11/day11.py
@@ -1,48 +1,53 @@
 #!/usr/bin/env python3
 
-from collections import Counter
+import attr
 import doctest
 import enum
 import sys
 import typing
 
+# https://www.redblobgames.com/grids/hexagons/: "odd-q" vertical layour
+
 
 @enum.unique
 class Direction(enum.Enum):
-    N = 0
-    NE = 1
-    SE = 2
-    S = 3
-    SW = 4
-    NW = 5
+    N = (0, -1)
+    NE = (1, -1)
+    SE = (1, 0)
+    S = (0, 1)
+    SW = (-1, 1)
+    NW = (-1, 0)
 
 
-def _simplify(path: typing.List[Direction]) -> typing.List[Direction]:
-    cnt = Counter(path)
+@attr.s
+class Hex:
+    q: int = attr.ib()  # "column"
+    r: int = attr.ib()  # "row"
 
-    # 1st pass: remove steps that cancel each other
-    for n in range(3):
-        dir_a = Direction(n)
-        dir_b = Direction(n + 3)
-        to_rm = min(cnt[dir_a], cnt[dir_b])
-        cnt[dir_a] -= to_rm
-        cnt[dir_b] -= to_rm
+    @staticmethod
+    def distance(a: 'Hex', b: 'Hex') -> int:
+        return (abs(a.q - b.q)
+                + abs(a.q + a.r - b.q - b.r)
+                + abs(a.r - b.r)) // 2
 
-    # 2nd pass: shortcuts! N + SE == NE, etc.
-    for n in range(6):
-        dir_a = Direction(n)
-        dir_b = Direction((n + 2) % 6)
-        dir_c = Direction((n + 1) % 6)
-        to_rm = min(cnt[dir_a], cnt[dir_b])
-        cnt[dir_a] -= to_rm
-        cnt[dir_b] -= to_rm
-        cnt[dir_c] += to_rm
+    def __add__(self, dir_: Direction) -> 'Hex':
+        if not isinstance(dir_, Direction):
+            raise TypeError(dir_)
+        return Hex(self.q + dir_.value[0],
+                   self.r + dir_.value[1])
 
-    # Back to a list!
-    path = []
-    for dir_, nb in cnt.items():
-        path += [dir_] * nb
-    return path
+    @property
+    def distance_from_origin(self):
+        zero = Hex(0, 0)
+        return Hex.distance(self, zero)
+
+
+def walk(data: str) -> typing.List[typing.Tuple[int, int]]:
+    """Return the coordinates after every step in the path."""
+    coords = Hex(0, 0)
+    for step in data.split(","):
+        coords += Direction[step.upper()]
+        yield coords
 
 
 def steps(data: str) -> int:
@@ -57,12 +62,14 @@ def steps(data: str) -> int:
     >>> steps('se,sw,se,sw,sw')
     3
     """
-    path = [Direction[step.upper()] for step in data.split(",")]
-    new_path = _simplify(path)
-    while new_path != path:
-        path = new_path
-        new_path = _simplify(path)
-    return len(path)
+    for coord in walk(data):
+        pass
+    return coord.distance_from_origin
+
+
+def max_distance(data: str) -> int:
+    """Max distance the boy got from the starting position."""
+    return max(coord.distance_from_origin for coord in walk(data))
 
 
 if __name__ == "__main__":
@@ -75,3 +82,4 @@ if __name__ == "__main__":
             data = fin.read().strip()
 
         print("Steps for %s: %d" % (fn, steps(data)))
+        print("Max distance for %s: %d" % (fn, max_distance(data)))
