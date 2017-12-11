0a12f0ae8159ae084473bac5725c2d1f74bd8087
Thomas Jost <schnouki@schnouki.net>
2017-12-11 11:11:40 +0100

    Day 9 part 2

--- day9/day9.py
+++ day9/day9.py
@@ -2,6 +2,7 @@
 
 import doctest
 import sys
+import typing
 
 
 def score(data: str) -> int:
@@ -28,12 +29,37 @@ def score(data: str) -> int:
     c = next(stream)
     if c != "{":
         raise ValueError(c)
-    return _score_group(stream, 1)
+    return _process_group(stream, 1)[0]
 
 
-def _score_group(stream, base) -> int:
-    """Compute the score for a group."""
-    score = base
+def count_garbage(data: str) -> int:
+    """Count the garbage in an input stream.
+
+    >>> count_garbage('{<>}')
+    0
+    >>> count_garbage('{<random characters>}')
+    17
+    >>> count_garbage('{<<<<>}')
+    3
+    >>> count_garbage('{<{!>}>}')
+    2
+    >>> count_garbage('{<!!>}')
+    0
+    >>> count_garbage('{<!!!>>}')
+    0
+    >>> count_garbage('{<{o"i!a,<{i<a>}')
+    10
+    """
+    stream = iter(data)
+    c = next(stream)
+    if c != "{":
+        raise ValueError(c)
+    return _process_group(stream, 1)[1]
+
+
+def _process_group(stream, base) -> typing.Tuple[int, int]:
+    """Compute the score and garbage count for a group."""
+    score, garbage = base, 0
     in_garbage = False
 
     while True:
@@ -43,12 +69,16 @@ def _score_group(stream, base) -> int:
                 in_garbage = False
             elif c == "!":
                 next(stream)
+            else:
+                garbage += 1
         elif c == "<":
             in_garbage = True
         elif c == "}":
-            return score
+            return score, garbage
         elif c == "{":
-            score += _score_group(stream, base + 1)
+            group_data = _process_group(stream, base + 1)
+            score += group_data[0]
+            garbage += group_data[1]
 
 
 if __name__ == "__main__":
@@ -63,3 +93,4 @@ if __name__ == "__main__":
             data = fin.read().strip()
 
         print("Score for %s: %d" % (fn, score(data)))
+        print("Garbage for %s: %d" % (fn, count_garbage(data)))
