23f2c1c7ae01f6131a51f0398536975883d7092f
Thomas Jost <schnouki@schnouki.net>
2018-12-05 20:53:39 +0100

    Day 5 part 2

--- 2018/day05/day05.scm
+++ 2018/day05/day05.scm
@@ -7,12 +7,12 @@
 (import (chicken string))
 (import srfi-1)
 
-(define all-pairs
-  (append-map (lambda (n)
-                (let* ((lchar (integer->char (+ n (char->integer #\a))))
-                       (uchar (char-upcase lchar)))
-                  (list (string lchar uchar) (string uchar lchar))))
-              (iota 26)))
+(define (one-pair n)
+  (let* ((lchar (integer->char (+ n (char->integer #\a))))
+         (uchar (char-upcase lchar)))
+    (list (string lchar uchar) (string uchar lchar))))
+
+(define all-pairs (append-map one-pair (iota 26)))
 
 (define reaction-regex (irregex-opt all-pairs))
 
@@ -28,9 +28,29 @@
 (define (solve-part1 data)
   (string-length (polymer-react/full data #t)))
 
+(define (remove-char s n)
+  (let ((char (integer->char (+ n (char->integer #\a)))))
+    (irregex-replace/all (irregex char 'case-insensitive) s)))
+
+(define (solve-part2 data)
+  (let* ((baseline (begin
+                    (print "Full reaction…")
+                    (polymer-react/full data)))
+         (shorts (map (lambda (n)
+                        (print "Processing " n)
+                        (polymer-react/full (remove-char baseline n)))
+                      (iota 26))))
+    (apply min (map string-length shorts))))
+
 (let ((test-data TEST-INPUT)
       (input-data (string-chomp (with-input-from-file "input" read-string))))
   (print "Part 1")
   (print "======")
   (print "TEST " (solve-part1 test-data))
-  (print "INPUT " (solve-part1 input-data)))
+  (print "INPUT " (solve-part1 input-data))
+
+  (print)
+  (print "Part 2")
+  (print "=======")
+  (print "TEST " (solve-part2 test-data))
+  (print "TEST " (solve-part2 input-data)))
