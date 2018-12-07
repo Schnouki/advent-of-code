c7d46fc668c505eaa7109ff225121a3532edf492
Thomas Jost <schnouki@schnouki.net>
2018-12-07 21:47:14 +0100

    Day 6 part 1

---
+++ 2018/day06/day06.scm
@@ -0,0 +1,87 @@
+#!/usr/bin/csi -script
+
+(define TEST-INPUT "1, 1
+1, 6
+8, 3
+3, 4
+5, 5
+8, 9")
+
+(import (chicken io))
+(import (chicken string))
+(import srfi-1)
+
+(define (parse-input data)
+  (map (lambda (line)
+         (let ((pair (string-split line ", ")))
+           (cons (string->number (car pair)) (string->number (cadr pair)))))
+       (string-split data "\n")))
+
+(define (min-x points)
+  (apply min (map car points)))
+(define (max-x points)
+  (apply max (map car points)))
+(define (min-y points)
+  (apply min (map cdr points)))
+(define (max-y points)
+  (apply max (map cdr points)))
+
+(define (dist point1 point2)
+  (+ (abs (- (car point1) (car point2)))
+     (abs (- (cdr point1) (cdr point2)))))
+
+(define (find-closest seeds point)
+  "Find which seed the point is the closest from."
+  (let* ((distances (map (lambda (seed) (dist point seed)) seeds))
+         (min-dist (apply min distances))
+         (find-dist (lambda (d) (= d min-dist)))
+         (nb-dists (count find-dist distances)))
+    (if (= nb-dists 1)
+        ;; Only 1: the seed!
+        (list-index find-dist distances)
+        ;; Several matches: too bad
+        #f)))
+
+(define (solve-part1 seeds)
+  "Compute the size of the areas for each seed, ignoring infinite areas."
+  ;; Inifite areas are guessed by examining which area "touch" an edge of the
+  ;; grid.
+  (let* ((width (+ 2 (max-x seeds)))
+         (height (+ 2 (max-y seeds)))
+         (find-seed
+          (lambda (c)
+            (find-closest seeds (cons (remainder c width)
+                                      (quotient c width)))))
+         (grid (list->vector (map find-seed (iota (* width height)))))
+         (grid-ref (lambda (c) (vector-ref grid c)))
+         (infinites (delete-duplicates
+                     (append
+                      (map grid-ref (iota width))
+                      (map grid-ref (iota width (* height (- width 1))))
+                      (map grid-ref (iota height 0 width))
+                      (map grid-ref (iota height (- width 1) width)))))
+         (counts (make-vector (length seeds) 0)))
+    ;; (print "GRID")
+    ;; (for-each (lambda (y)
+    ;;             (print (subvector grid (* width y) (* width (+ 1 y)))))
+    ;;           (iota height))
+    ;; (print "INIFINITES " infinites)
+    (for-each (lambda (a)
+                (when a
+                  (vector-set! counts a
+                               (+ 1 (vector-ref counts a)))))
+              (vector->list grid))
+    (apply max
+           (map third
+                (filter (lambda (z) (not (member (car z) infinites)))
+                        (zip (iota (length seeds))
+                             seeds
+                             (vector->list counts)))))))
+
+
+(let ((test-data (parse-input TEST-INPUT))
+      (input-data (parse-input (string-chomp (with-input-from-file "input" read-string)))))
+  (print "Part 1")
+  (print "======")
+  (print "TEST " (solve-part1 test-data))
+  (print "INPUT " (solve-part1 input-data)))
