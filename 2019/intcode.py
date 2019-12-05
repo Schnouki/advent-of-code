a4996b71c6d0097ccb9e454e3ee90ba3aaabdab2
Thomas Jost <schnouki@schnouki.net>
2019-12-05 10:49:20 +0100

    Day 5: add parameter modes to intcode.Computer

--- 2019/intcode.py
+++ 2019/intcode.py
@@ -1,45 +1,84 @@
+import enum
+import functools
+import inspect
 from typing import List
 
 import attr
 
 
+class ParameterMode(enum.Enum):
+    POSITION = 0
+    IMMEDIATE = 1
+
+
 @attr.s
 class Computer:
     mem: List[int] = attr.ib()
+    inputs: List[int] = attr.ib(factory=lambda: [])
+    outputs: List[int] = attr.ib(factory=lambda: [])
     ip: int = attr.ib(default=0)
     halted: bool = attr.ib(default=False)
 
     def copy(self):
-        return self.__class__(self.mem[:], 0, False)
+        return self.__class__(self.mem[:], self.inputs[:], self.outputs[:], 0, False)
 
     def run(self):
         while not self.halted:
             self.step()
 
     def step(self):
+        """Run a single step of the program."""
         if self.halted:
             return
-        op = self.mem[self.ip]
 
+        op_str = str(self.mem[self.ip])
+        func = self.decode_op(op_str)
+        func(self)
+
+    @classmethod
+    @functools.lru_cache
+    def decode_op(cls, op_str):
+        op = int(op_str[-2:])
+
+        # Find opcode function
         op_func_name = f"op_{op}"
-        if not hasattr(self, op_func_name):
-            raise ValueError(f"Invalid opcode {op} at address {self.ip}")
+        if not hasattr(cls, op_func_name):
+            raise ValueError(f"Invalid opcode {op}")
+        op_func = getattr(cls, op_func_name)
 
-        self.ip += getattr(self, op_func_name)()
+        # How many parameters does it have?
+        sig = inspect.signature(op_func)
+        nb_params = (len(sig.parameters) - 1) >> 1
 
-    def op_1(self):
-        """Addition. *(ip+3) = *(ip+1) + *(ip+2)."""
-        addrs = self.mem[self.ip + 1 : self.ip + 4]
-        self.mem[addrs[2]] = self.mem[addrs[0]] + self.mem[addrs[1]]
-        return 4
+        # Get modes
+        op_str = op_str.zfill(nb_params + 2)
+        modes = [ParameterMode(int(d)) for d in op_str[:-2]]
 
-    def op_2(self):
-        """Multiplication. *(ip+3) = *(ip+1) * *(ip+2)."""
-        addrs = self.mem[self.ip + 1 : self.ip + 4]
-        self.mem[addrs[2]] = self.mem[addrs[0]] * self.mem[addrs[1]]
-        return 4
+        # Prepare function to call
+        def func(self):
+            params = []
+            for idx, mode in enumerate(modes[::-1], 1):
+                params += [mode, self.mem[self.ip + idx]]
+            op_func(self, *params)
+            self.ip += nb_params + 1
+
+        return func
+
+    def get(self, mode, value):
+        if mode == ParameterMode.POSITION:
+            return self.mem[value]
+        if mode == ParameterMode.IMMEDIATE:
+            return value
+        raise ValueError(f"Invalid parameter mode {mode}")
+
+    def op_1(self, m1, v1, m2, v2, m3, v3):
+        """Addition. v3 = v1 + v2"""
+        self.mem[v3] = self.get(m1, v1) + self.get(m2, v2)
+
+    def op_2(self, m1, v1, m2, v2, m3, v3):
+        """Multiplication. v3 = v1 * v2"""
+        self.mem[v3] = self.get(m1, v1) * self.get(m2, v2)
 
     def op_99(self):
         """Halt."""
         self.halted = True
-        return 1
