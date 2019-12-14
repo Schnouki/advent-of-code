92259816c572deffe98fa3faa5659fc00b5777e7
Thomas Jost <schnouki@schnouki.net>
2019-12-14 22:59:18 +0100

    Day 14 part 2

--- 2019/day14.py
+++ 2019/day14.py
@@ -68,7 +68,8 @@ class System:
         for input in rec.inputs:
             self.inventory[input.name] -= nb * input.qty
 
-    def make(self, name: str, qty: int):
+    def make(self, name: str, qty: int) -> int:
+        self.inventory.clear()
         self.make_one(name, qty)
 
         needed = self.needed
@@ -78,22 +79,60 @@ class System:
             self.make_one(name, qty)
             needed = self.needed
 
+        return -self.inventory["ORE"]
+
+    def make_max(self, max_ore: int) -> int:
+        cur_try, min_try, max_try = 1, 1, 1
+        res = 0
+        while True:
+            max_try = cur_try * 2
+            res = self.make("FUEL", cur_try)
+            if res > max_ore:
+                break
+            min_try = cur_try
+            cur_try *= 2
+            max_try = cur_try
+
+        while max_try - min_try > 1:
+            cur_try = (min_try + max_try) // 2
+            res = self.make("FUEL", cur_try)
+            if res == max_ore:
+                break
+            elif res > max_ore:
+                max_try = cur_try
+            else:
+                min_try = cur_try
+
+        fuel, ore = self.inventory["FUEL"], -self.inventory["ORE"]
+        if ore <= max_ore:
+            return fuel
+        else:
+            return fuel - 1
+
 
 class Day14(Puzzle):
     test_data = [
-        """10 ORE => 10 A
-1 ORE => 1 B
-7 A, 1 B => 1 C
-7 A, 1 C => 1 D
-7 A, 1 D => 1 E
-7 A, 1 E => 1 FUEL""",
-        """9 ORE => 2 A
-8 ORE => 3 B
-7 ORE => 5 C
-3 A, 4 B => 1 AB
-5 B, 7 C => 1 BC
-4 C, 1 A => 1 CA
-2 AB, 3 BC, 4 CA => 1 FUEL""",
+        """157 ORE => 5 NZVS
+165 ORE => 6 DCFZ
+44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+179 ORE => 7 PSHF
+177 ORE => 5 HKGWZ
+7 DCFZ, 7 PSHF => 2 XJWVT
+165 ORE => 2 GPVTF
+3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT""",
+        """2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
+17 NVRVD, 3 JNWZP => 8 VPVL
+53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
+22 VJHF, 37 MNCFX => 5 FWMGM
+139 ORE => 4 NVRVD
+144 ORE => 7 JNWZP
+5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
+5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
+145 ORE => 6 MNCFX
+1 NVRVD => 8 CXFTF
+1 VJHF, 6 MNCFX => 4 RFSQX
+176 ORE => 6 VJHF""",
         """171 ORE => 8 CNZTR
 7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
 114 ORE => 4 BHXH
@@ -112,7 +151,8 @@ class Day14(Puzzle):
 7 XCVML => 6 RJRHP
 5 BHXH, 4 VRPVC => 5 LTCX""",
     ]
-    test_result_part1 = ["31", "165", "2210736"]
+    test_result_part1 = ["13312", "180697", "2210736"]
+    test_result_part2 = ["82892753", "5586022", "460664"]
 
     def prepare_data(self, data):
         receipts = []
@@ -121,8 +161,11 @@ class Day14(Puzzle):
         return System(receipts)
 
     def run_part1(self, system: System):
-        system.make("FUEL", 1)
-        return str(-system.inventory["ORE"])
+        return str(system.make("FUEL", 1))
+
+    def run_part2(self, system: System):
+        goal = 1_000_000_000_000
+        return str(system.make_max(goal))
 
 
 run(obj=Day14())
