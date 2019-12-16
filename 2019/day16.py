3ce7f68ea33ce9805b8f670598208e539c2f3cda
Thomas Jost <schnouki@schnouki.net>
2019-12-16 12:15:25 +0100

    Day 16 part 1 optimized

--- 2019/day16.py
+++ 2019/day16.py
@@ -1,27 +1,22 @@
 #!/usr/bin/env python3
 
+import itertools as it
 from typing import List
+
+import numpy as np
 
 from aoc import Puzzle, run
 
 
-def fft_phase(data: List[int]) -> List[int]:
-    """Compute one phase of Flawed Frequency Transmission
+BASE_PATTERN = (0, 1, 0, -1)
 
-    >>> fft_phase([1, 2, 3, 4, 5, 6, 7, 8])
-    [4, 8, 2, 2, 6, 1, 5, 8]
-    >>> fft_phase([4, 8, 2, 2, 6, 1, 5, 8])
-    [3, 4, 0, 4, 0, 4, 3, 8]
-    """
-    output = []
-    for pos in range(1, len(data) + 1):
-        pattern = ([0] * pos) + ([1] * pos) + ([0] * pos) + ([-1] * pos)
-        rep = (len(data) + 1) // len(pattern)
-        pattern += pattern * rep
-        pattern = pattern[1:]
-        m = sum(data[i] * pattern[i] for i in range(len(data)))
-        output.append(int(str(m)[-1]))
-    return output
+
+def pattern(pos: int):
+    pattern = it.cycle(BASE_PATTERN)
+    while True:
+        v = next(pattern)
+        for _ in range(pos):
+            yield v
 
 
 class Day16(Puzzle):
@@ -42,9 +37,18 @@ class Day16(Puzzle):
         return [int(c) for c in data.strip()]
 
     def run_part1(self, data: List[int]) -> str:
+        matrix = np.matrix(
+            [
+                list(it.islice(pattern(pos), 1, len(data) + 1))
+                for pos in range(1, len(data) + 1)
+            ]
+        )
+        vector = np.array(data)
         for _ in range(100):
-            data = fft_phase(data)
-        return "".join(str(n) for n in data[:8])
+            res = matrix @ vector
+            res = [int(str(m)[-1]) for m in res.tolist()[0]]
+            vector = np.array(res)
+        return "".join(str(n) for n in res[:8])
 
 
 run(obj=Day16())
