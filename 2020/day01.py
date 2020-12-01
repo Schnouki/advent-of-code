a6b776e499a68206510489048ff46b69452ac356
Thomas Jost <schnouki@schnouki.net>
2020-12-01 13:35:03 +0100

    Day 1: optimize it

--- 2020/day01.py
+++ 2020/day01.py
@@ -12,18 +12,20 @@ class Day01(Puzzle):
     test_result_part2 = [241861950]
 
     def prepare_data(self, data):
-        return [int(line) for line in data.splitlines()]
+        return set(int(line) for line in data.splitlines())
 
     def run_part1(self, data):
         # Find a pair that sums to 2020, and return its product
-        for a, b in it.combinations(data, 2):
-            if a + b == 2020:
+        for a in data:
+            b = 2020 - a
+            if b in data:
                 return a*b
 
     def run_part2(self, data):
         # Find 3 numbers that sum to 2020, and return their product
-        for a, b, c in it.combinations(data, 3):
-            if a + b + c == 2020:
+        for a, b in it.combinations(data, 2):
+            c = 2020 - a - b
+            if c in data:
                 return a*b*c
 
 
